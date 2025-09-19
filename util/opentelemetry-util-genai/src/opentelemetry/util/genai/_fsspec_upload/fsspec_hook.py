fa08239d1c6e25b7ba06a40459942775abeb3c78
Keith Decker <kdecker@splunk.com>
2025-09-19 09:22:37 -0600

    Update utils to match inference PR

--- util/opentelemetry-util-genai/src/opentelemetry/util/genai/_fsspec_upload/fsspec_hook.py
+++ util/opentelemetry-util-genai/src/opentelemetry/util/genai/_fsspec_upload/fsspec_hook.py
@@ -22,27 +22,15 @@ import threading
 from concurrent.futures import Future, ThreadPoolExecutor
 from dataclasses import asdict, dataclass
 from functools import partial
-from typing import Any, Callable, Final, Literal, TextIO, cast
+from typing import Any, Callable, Literal, TextIO, cast
 from uuid import uuid4
 
 import fsspec
 
 from opentelemetry._logs import LogRecord
-from opentelemetry.semconv._incubating.attributes import gen_ai_attributes
 from opentelemetry.trace import Span
 from opentelemetry.util.genai import types
 from opentelemetry.util.genai.upload_hook import UploadHook
-
-GEN_AI_INPUT_MESSAGES_REF: Final = (
-    gen_ai_attributes.GEN_AI_INPUT_MESSAGES + "_ref"
-)
-GEN_AI_OUTPUT_MESSAGES_REF: Final = (
-    gen_ai_attributes.GEN_AI_OUTPUT_MESSAGES + "_ref"
-)
-GEN_AI_SYSTEM_INSTRUCTIONS_REF: Final = (
-    gen_ai_attributes.GEN_AI_SYSTEM_INSTRUCTIONS + "_ref"
-)
-
 
 _logger = logging.getLogger(__name__)
 
@@ -189,19 +177,7 @@ class FsspecUploadHook(UploadHook):
             },
         )
 
-        # stamp the refs on telemetry
-        references = {
-            GEN_AI_INPUT_MESSAGES_REF: ref_names.inputs_ref,
-            GEN_AI_OUTPUT_MESSAGES_REF: ref_names.outputs_ref,
-            GEN_AI_SYSTEM_INSTRUCTIONS_REF: ref_names.system_instruction_ref,
-        }
-        if span:
-            span.set_attributes(references)
-        if log_record:
-            log_record.attributes = {
-                **(log_record.attributes or {}),
-                **references,
-            }
+        # TODO: stamp the refs on telemetry
 
     def shutdown(self) -> None:
         # TODO: support timeout
